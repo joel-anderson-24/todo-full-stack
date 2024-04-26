199f76f9537f30f861d82b7be15f3bd7b93469ee
Joel Anderson <janderson56789@gmail.com>
2024-04-26 15:58:26 +1200

    Add models for todos

---
+++ models/todos.ts
@@ -0,0 +1,12 @@
+export interface Todos {
+  id: number
+  task: string
+  priority: string
+  completed: boolean
+}
+
+export interface TodoData {
+  task: string
+  priority: string
+  completed: boolean
+}
